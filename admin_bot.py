14fe327ef81b243b2da9403e036d2241ac4b7fca
Sergej Bestuzhev <sergej.bestuzhev@gmail.com>
2024-02-06 21:30:04 +0300

    Migration of the project to aiogram 3

--- admin_bot.py
+++ admin_bot.py
@@ -1,16 +1,24 @@
-from aiogram import Bot, Dispatcher, executor 
+import asyncio
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import ChatMemberUpdated
+from aiogram.filters import ChatMemberUpdatedFilter, IS_MEMBER, IS_NOT_MEMBER
 from dotenv import dotenv_values
 
 # Получаем TOKEN бота из переменной окружения
 config = dotenv_values(".env")
 
 bot = Bot(config.get("BOT_TOKEN"))
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler(content_types=["new_chat_members"])
-async def new_member(message):
-    await bot.send_message(message.chat.id, "@" + str(message.from_user.username) + " hello bro!")
+@dp.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
+async def on_user_join(event: ChatMemberUpdated):
+    await event.answer(text=f"Привет! Спасибо, что добавили меня")   
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    asyncio.run(main())
